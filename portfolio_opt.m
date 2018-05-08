5c724903db82dd0579a7b43e9507d374df059d36
terkelolsen <terkel.olsen@epfl.ch>
2018-05-08 20:26:24 +0200

    some assertations on the first solution

--- portfolio_opt.m
+++ portfolio_opt.m
@@ -26,18 +26,52 @@ mu = linspace(min(AssetMean),max(AssetMean),20);
 for i = 1:length(mu)
     cvx_begin quiet
     variable x(length(AssetMean))
+    dual variable lambda;
+    dual variable nu1;
+    dual variable nu2;
     y = x'*AssetCovar*x;
     minimize (y);
     
     subject to
-    x >= 0;
-    ones(1,length(AssetMean))*x==1;
-    AssetMean'*x == mu(i);
+    -x <= 0 : lambda;
+    mu(i) - AssetMean'*x == 0 : nu1;
+    1 - ones(1,length(AssetMean))*x  == 0 : nu2;
     cvx_end
     
     x_out(:,i) = x;
     y_out(i) = y;
+    
+    %assertations
+    z = lambda - nu1*AssetMean - nu2;
+    dual_value = -1/4*(z')*AssetCovar^(-1)*z - nu1*mu(i) - nu2;
+
+    %check the x value
+    x_check = 1/2*AssetCovar^(-1)*z;
+
+    %assert for mistakes
+    assert(norm(x-x_check)<1e-3)
+    assert(norm(y - dual_value)<1e-6)
+    
+    %lagrangian derivative == 0
+    assert(max(2*AssetCovar*x-lambda+nu1*AssetMean+nu2)<1e-5)
+    
+    
 end
+
+cvx_begin quiet
+variable x(length(AssetMean))
+dual variable lambda;
+dual variable nu1;
+dual variable nu2;
+ylow = x'*AssetCovar*x;
+minimize (ylow);
+
+subject to
+-x <= 0 : lambda;
+AssetMean'*x >=0 : nu1;
+1 - ones(1,length(AssetMean))*x  == 0 : nu2;
+cvx_end
+
 figure(2)
 plot(diag(sqrt(AssetCovar)),AssetMean,'r.')
 text(diag(sqrt(AssetCovar)), AssetMean, AssetList, 'horizontal','left', 'vertical','bottom','fontsize',18)
@@ -47,7 +81,10 @@ xlim([0 0.75])
 ylim([-0.05 0.25])
 hold on
 plot(sqrt(y_out),mu,'b--')
+plot(sqrt(ylow),AssetMean'*x,'g*')
 saveFigures(gcf,'asset-efficient-frontier',false)
+
+
 
 %% Introduce the shorting problem
 clear y_out
